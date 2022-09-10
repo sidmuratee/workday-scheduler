0f02de77d4e3bcf7a0364a4bd3a4b5a8f5df81c8
smurtee <muratees@yahoo.com>
2022-09-10 15:59:13 -0500

    localstorage + color change

--- script.js
+++ script.js
@@ -20,6 +20,25 @@ function setTime() {
 
 setTime();
 
+function timeBlockColor (){
+    let currentTime = moment().hours()
+    let timeBlock = $(".timeSlot")
+    timeBlock.each(function (){
+        let hour = parseInt($(this).attr("data-id"))
+        if (hour === currentTime){
+            $(this).children(".col-10").attr("class", "present col-10")
+            
+        } else if (currentTime > hour){
+            $(this).children(".col-10").attr("class", "past col-10")
+ 
+        } else {
+            $(this).children(".col-10").attr("class", "future col-10")
+        }
+    })
+}
+
+timeBlockColor();
+
 $(".saveBtn").on("click", function(){
     
     let text = $(this).siblings('textarea').val();
@@ -29,4 +48,12 @@ $(".saveBtn").on("click", function(){
     })
 
 
-
+$("#timeBlockContent9").text(localStorage.getItem("9"))
+$("#timeBlockContent10").text(localStorage.getItem("10"))
+$("#timeBlockContent11").text(localStorage.getItem("11"))
+$("#timeBlockContent12").text(localStorage.getItem("12"))
+$("#timeBlockContent13").text(localStorage.getItem("13"))
+$("#timeBlockContent14").text(localStorage.getItem("14"))
+$("#timeBlockContent15").text(localStorage.getItem("15"))
+$("#timeBlockContent16").text(localStorage.getItem("16"))
+$("#timeBlockContent17").text(localStorage.getItem("17"))
